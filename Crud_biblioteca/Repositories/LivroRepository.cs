cf46e337584ece6568f37ebbc15aa225d7e30985
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-08-20 20:28:15 -0300

    feat:Implementa interfaces nas camadas repository e service

---
+++ Crud_biblioteca/Repositories/LivroRepository.cs
@@ -0,0 +1,108 @@
+using Crud_biblioteca.Model;
+using Crud_biblioteca.DATA;
+using Dapper;
+
+
+namespace Crud_biblioteca.Repository
+{
+    internal class LivroRepository : ILivroRepositoryInterface
+    {
+
+       public bool Inserir(Livro livro)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @"INSERT INTO livros (nome, quantidade, valor)
+                            VALUES (@nome, @quantidade, @valor)";
+
+            var result = conn.Conexao.Execute(query, livro);
+
+            return result == 1;
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = "SELECT * FROM livros WHERE id = @id";
+
+            Livro livro = conn.Conexao.Query<Livro>(query, new { id }).FirstOrDefault();
+
+            return livro;
+
+        }
+
+        public List<Livro> ListarLivros()
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @" 
+                           SELECT * 
+                           FROM livros";
+
+            return conn.Conexao.Query<Livro>(query).ToList();
+        }
+
+        public bool Atualizar(Livro livro)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @"
+                            UPDATE livros
+                            SET
+                                nome = @Nome,
+                                quantidade = @Quantidade,
+                                valor = @Valor
+                            WHERE id = @Id";
+
+            var result = conn.Conexao.Execute(query, livro);
+            
+            return result == 1;
+        }
+
+        public bool AtualizarEstoque(int id, int quantidade)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @"
+                           UPDATE livros
+                            SET
+                                quantidade = @quantidade
+                            Where id = @id";
+
+            var result = conn.Conexao.Execute(query, new {id, quantidade});
+
+            return result == 1;
+        }
+
+        public bool AtualizarValor(int id, double valor)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @"
+                           UPDATE livros
+                            SET
+                                valor = @valor
+                            Where id = @id";
+
+            var result = conn.Conexao.Execute(query, new { id, valor });
+
+            return result == 1;
+        }
+        public bool Deletar(int id)
+        {
+            using var conn = new ConexaoBD();
+
+            string query = @"
+                            DELETE FROM livros 
+                            WHERE 
+                                id = @id";
+
+            var result = conn.Conexao.Execute(query, new { id });
+
+            return result == 1;
+        }
+
+
+    }
+}
---
+++ Crud_biblioteca/Services/LivroService.cs
@@ -0,0 +1,127 @@
+﻿using Crud_biblioteca.Model;
+using Crud_biblioteca.Repository;
+
+namespace Crud_biblioteca.Service
+{
+    internal class LivroService : ILivroServiceInterface
+    {
+        private readonly LivroRepository _livroRepository;
+        public LivroService()
+        {
+            _livroRepository = new LivroRepository();
+ 
+        }
+
+        public Livro Inserir(Livro livro) 
+        {
+            try
+            {
+                var id = _livroRepository.BuscarPorId(livro.Id);
+                if (id == null)
+                {
+                    Console.WriteLine("Erro: Id inválido");
+                    return null;
+                }
+
+                if (livro.Nome.IsWhiteSpace())
+                {
+                    Console.WriteLine("Nome do livro vazio. Digite um nome válido");
+                    return null;
+                }
+
+                if (livro.Quantidade < 0 || livro.Quantidade == null)
+                {
+                    Console.WriteLine("Erro: Um livro não poder ter uma quantidade negativa");
+                    return null;
+                }
+
+                if (livro.Valor < 0)
+                {
+                    Console.WriteLine("Erro: O valor do livro não poder ser negativo");
+                    return null;
+                }
+
+                _livroRepository.Inserir(livro);
+
+                return livro;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+
+        }
+
+        public List<Livro> ListarLivros()
+        {
+            var livros = _livroRepository.ListarLivros();
+
+            if(livros == null) 
+            { 
+                return null; 
+            }
+
+            return livros;
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            try
+            {
+                var livro = _livroRepository.BuscarPorId(id);
+                if (livro == null)
+
+                {
+                    Console.WriteLine("Livro não encontrado. Informe um Id válido.");
+                    return null;
+                }
+                return livro;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+                return null;
+            }
+
+        }
+
+        public Livro AtualizarLivro(Livro livro)
+        {
+            try
+            {
+                if(livro.Quantidade < 0)
+                {
+                    Console.WriteLine("Erro: A quantidade de livros não pode ser menor que zero");
+                }
+
+                if(livro.Valor < 0)
+                {
+                    Console.WriteLine("Erro: O valor do livro não pode ser menor que zero");
+                }
+
+                _livroRepository.Atualizar(livro);
+
+                return livro;
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+
+        }
+
+        public int AtualizarEstoque(int id,int estoque)
+        {
+            if(estoque > 0)
+            {
+                Console.WriteLine("Erro: O estoque não pode ser um valor menor que zero");
+            }
+
+            _livroRepository.AtualizarEstoque(id, estoque);
+
+            return estoque;
+
+        }
+    }
+}
