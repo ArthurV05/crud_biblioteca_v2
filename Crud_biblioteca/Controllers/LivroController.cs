cb0ba57afbb9c064941e940fe3095afc2e2d900a
pedroarthurvalente-design <pedroarthurvalente@gmail.com>
2026-09-07 12:45:06 -0300

    refactor: Melhora o tratamento de valores substituindo parse por tryParse e Cria tratamento de erros para erro de conexão com o banco de dados

--- Crud_biblioteca/Controllers/LivroController.cs
+++ Crud_biblioteca/Controllers/LivroController.cs
@@ -1,4 +1,6 @@
-﻿using Crud_biblioteca.Model;
+﻿using System.IO.Pipelines;
+using System.Text;
+using Crud_biblioteca.Model;
 using Crud_biblioteca.Service;
 using Crud_biblioteca.UI;
 
@@ -21,14 +23,14 @@ namespace Crud_biblioteca.Controllers
             while (true)
             {
                 LivroMenu.Menu();
-                int escolha = 0;
-                try
+                int escolha;
+
+                while (!int.TryParse(Console.ReadLine(), out escolha))
                 {
-                    escolha = int.Parse(Console.ReadLine());
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Erro: Você precisa digitar um número inteiro.");
+                    Task.Delay(3000).Wait();
+                    LivroMenu.Menu();
+
                 }
 
                 switch (escolha)
@@ -59,104 +61,193 @@ namespace Crud_biblioteca.Controllers
                         break;
                 }
             }
-
         }
 
-
-        public void InserirLivro() //Testar TryParse, posteriormente, espalhar para todo o controller
+        public void InserirLivro() //Fazer try catch nos outros casos de conexão ao banco de dados
         {
-            //try
-            //{
+
             Console.WriteLine("Digite o nome do livro:");
             string nome = Console.ReadLine();
 
             Console.WriteLine("Dite a quantidade de livros: ");
-            int quantidade = int.TryParse(Console.ReadLine().ToString());
+            int quantidade;
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out quantidade))
+                {
+                    break;
+                }
+                Console.WriteLine("Erro: O campo quantidade só aceita valores númericos");
+                Console.WriteLine("Dite a quantidade de livros: ");
+            }
 
             Console.WriteLine("Dite o valor do livro: ");
-            double valor = double.Parse(Console.ReadLine());
+            decimal valor;
 
-            Livro livro = new Livro(0, nome, quantidade, valor);
-
-            var result = _livroService.Inserir(livro);
-
-            if (result == true)
+            while (true)
             {
-                Console.WriteLine("livro registrado com sucesso!");
+                string? input = Console.ReadLine();
+
+                if (decimal.TryParse(input, out valor))
+                {
+                    break;
+                }
+                Console.WriteLine("Erro: O campo valor só aceita valores númericos");
+                Console.WriteLine("Dite o valor do livro: ");
             }
-            else
+            try
             {
-                Console.WriteLine("Valores incorretos. Informe corretamente as informações para cadastro dos livros");
+                Livro livro = new(0, nome, quantidade, valor);
+
+                var result = _livroService.Inserir(livro);
+
+                if (result == true)
+                {
+                    Console.WriteLine("livro registrado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Valores incorretos. Informe corretamente as informações para cadastro dos livros");
+                }
             }
-            // }
-            // catch (Exception ex)
-            // {
-            //     Console.WriteLine($"Erro: {ex.Message}");
-            // }
+            catch (Npgsql.NpgsqlException)
+            {
+                Console.WriteLine("Não foi possível concluir a operação no banco de dados.");
+            }
+
 
         }
 
         public void ListarLivros()
         {
-            var livros = _livroService.ListarLivros();
-
-            foreach (var livro in livros)
+            try
             {
-                Console.WriteLine(livro.ToString());
+                var livros = _livroService.ListarLivros();
+
+                foreach (var livro in livros)
+                {
+                    Console.WriteLine(livro.ToString());
+                }
             }
+            catch (Npgsql.NpgsqlException)
+            {
+                Console.WriteLine("Não foi possível concluir a operação no banco de dados.");
+            }
+
         }
 
         public void BuscarLivroPorId()
         {
+
             Console.WriteLine("Informe o ID do livro que deseja buscar: ");
+
+            int id;
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (int.TryParse(input, out id))
+                {
+                    break;
+                }
+                Console.WriteLine("Erro: O id precisa ser um numeral");
+                Task.Delay(2000).Wait();
+                Console.WriteLine("Informe o ID do livro que deseja buscar: ");
+            }
+
+
             try
             {
-                int id = int.Parse(Console.ReadLine());
-
                 var livro = _livroService.BuscarPorId(id);
 
-                if (livro != null)
-                    Console.WriteLine(livro.ToString());
+                if (livro == null)
+                {
+                    Console.WriteLine("Livro não encontrado, informe um id válido");
+                    Task.Delay(2000).Wait();
+                    return;
+                }
+
+                Console.WriteLine(livro.ToString());
             }
-            catch (Exception ex)
+            catch (Npgsql.NpgsqlException)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Não foi possível concluir a operação no banco de dados.");
             }
         }
 
-        public void AtualizarLivro() //Refatorar controller 
+        public void AtualizarLivro()
         {
             bool continuar = true;
 
             Console.WriteLine("Informe o id do livro que será atualizado");
 
-            int id = int.Parse(Console.ReadLine());
-            var livro = _livroService.BuscarPorId(id);
+            int id;
 
-            if (livro == null)
-                continuar = false;
-
-            while (continuar)
+            while (!int.TryParse(Console.ReadLine(), out id))
             {
-                LivroMenu.MenuAtualizar();
-                int opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine("Erro: O id precisa ser um número inteiro");
+                Console.WriteLine("Informe o id do livro que será atualizado");
+            }
 
-                switch (opcao)
+            try
+            {
+                var livro = _livroService.BuscarPorId(id);
+
+                if (livro == null)
                 {
-                    case 1:
-                        {
-                            try
+                    Console.WriteLine("O id informado é invalido. Digite um id correto e tente novamente");
+                    Task.Delay(2000).Wait();
+                    continuar = false;
+                }
+                while (continuar)
+                {
+                    LivroMenu.MenuAtualizar();
+                    int opcao;
+
+                    while (!int.TryParse(Console.ReadLine(), out opcao))
+                    {
+                        Console.WriteLine("Erro: Você precisa informar um número inteiro.");
+                        LivroMenu.MenuAtualizar();
+                    }
+
+                    switch (opcao)
+                    {
+                        case 1:
                             {
+
                                 Console.WriteLine("Informe o novo nome");
                                 string novoNome = Console.ReadLine();
 
                                 Console.WriteLine("Informe a quantidade");
-                                int quantidade = int.Parse(Console.ReadLine());
+                                int quantidade;
+                                while (true)
+                                {
+                                    string? input = Console.ReadLine();
 
-                                Console.WriteLine("Informe o novo valor");
-                                double novoValor = double.Parse(Console.ReadLine());
+                                    if (!int.TryParse(input, out quantidade))
+                                    {
+                                        Console.WriteLine("Erro: O campo quantidade precisa ser preenchido por um número inteiro");
+                                        Console.WriteLine();
+                                        Console.WriteLine("Informe a quantidade");
+                                        continue;
+                                    }
 
-                                Livro novoLivro = new Livro(livro.Id, novoNome, quantidade, novoValor);
+                                    break;
+                                }
+
+                                Console.WriteLine("Informe o novo valor:");
+
+                                decimal novoValor;
+
+                                while (!decimal.TryParse(Console.ReadLine(), out novoValor))
+                                {
+                                    Console.WriteLine("Erro: O campo valor precisa ser preenchido por um numeral");
+                                    Console.WriteLine();
+                                    Console.WriteLine("Informe o novo valor:");
+                                }
+
+                                Livro novoLivro = new(livro.Id, novoNome, quantidade, novoValor);
 
                                 var result = _livroService.AtualizarLivro(novoLivro);
 
@@ -170,22 +261,22 @@ namespace Crud_biblioteca.Controllers
                                     Console.WriteLine("Erros no preenchimento dos campos. Informe todos os campos corretamente");
                                 }
 
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex.Message);
+                                break;
                             }
 
-                            break;
-                        }
-
-                    case 2:
-                        {
-                            try
+                        case 2:
                             {
-                                Console.WriteLine("Informe o novo número em estoque");
+                                Console.WriteLine("Informe o novo número de livros em estoque");
+                                int novoEstoque;
 
-                                int novoEstoque = int.Parse(Console.ReadLine());
+                                string? input = Console.ReadLine();
+
+                                while (!int.TryParse(input, out novoEstoque))
+                                {
+                                    Console.WriteLine("O valor do estoque precisa ser um número ");
+                                    Console.WriteLine("Informe o novo número em estoque:");
+                                    input = Console.ReadLine();
+                                }
 
                                 var result = _livroService.AtualizarEstoque(livro.Id, novoEstoque);
 
@@ -201,23 +292,27 @@ namespace Crud_biblioteca.Controllers
                                     break;
                                 }
 
+
                             }
-                            catch (Exception ex)
+
+                        case 3:
                             {
-                                Console.WriteLine(ex.Message);
-                                break;
-                            }
-                        }
+                                decimal valor;
 
-                    case 3:
-                        {
-                            try
-                            {
-                                Console.WriteLine("Informe o novo valor");
+                                while (true)
+                                {
+                                    Console.WriteLine("Informe o valor do livro:");
+                                    string? input = Console.ReadLine();
 
-                                var valorNovo = decimal.Parse(Console.ReadLine());
-                                var result = _livroService.AtualizarValor(livro.Id, valorNovo);
+                                    if (decimal.TryParse(input, out valor))
+                                    {
+                                        break;
+                                    }
 
+                                    Console.WriteLine("O valor precisa ser um número.");
+                                }
+
+                                var result = _livroService.AtualizarValor(livro.Id, valor);
                                 if (result == true)
                                 {
                                     Console.WriteLine("Valor atualizado com sucesso!");
@@ -229,40 +324,56 @@ namespace Crud_biblioteca.Controllers
                                     Console.WriteLine("Erro: O valor não pode ser negativo");
                                     break;
                                 }
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine(ex.Message);
-                                break;
+
                             }
 
-                        }
-
-                    case 4:
-                        continuar = false;
-                        Console.Clear();
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida");
-                        break;
+                        case 4:
+                            continuar = false;
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            break;
+                    }
                 }
             }
+            catch (Npgsql.NpgsqlException)
+            {
+                Console.WriteLine("Não foi possível concluir a operação no banco de dados.");
+
+            }
+
         }
 
-        public void DeletarLivro() //Colocar uma validação para o id (tryparse?)
+        public void DeletarLivro()
         {
-            Console.WriteLine("Informe o id do livro que será excluido");
-            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o Id do livro:");
+            int id;
 
-            var result = _livroService.DeletarLivro(id);
-            if (result == true)
+            while (!int.TryParse(Console.ReadLine(), out id))
             {
-                Console.WriteLine("Livro apagado com sucesso!");
+
+                Console.WriteLine("Id Inválido. Informe um número válido");
+                Console.WriteLine("Informe o Id do livro");
             }
-            else
+            try
             {
-                Console.WriteLine("Não foi possível apagar o livro. Certifique-se que o id esteja correto");
+                var result = _livroService.DeletarLivro(id);
+                if (result == true)
+                {
+                    Console.WriteLine("Livro apagado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Não existe um livro com este número de ID. Informe um livro existente");
+                }
             }
+            catch (Npgsql.NpgsqlException)
+            {
+                Console.WriteLine("Não foi possível concluir a operação no banco de dados.");
+
+            }
+
         }
 
         public void SairSistema()
